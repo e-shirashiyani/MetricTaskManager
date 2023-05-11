09a9c041482a3f6110d7c2a307692dce4c1a6ca4
e.shirashiyani <e.shirashiyani>
2023-05-11 12:50:01 +0430

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by e.shirashiyani on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
