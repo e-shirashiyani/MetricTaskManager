b747d3f5eab52525c1ff7bc01559123f62d9a466
e.shirashiyani <e.shirashiyani>
2023-05-12 11:11:15 +0430

    added viewModel for handling operation tasks

--- TaskManager/Task.swift
+++ TaskManager/Task.swift
@@ -4,5 +4,12 @@
 //
 //  Created by e.shirashiyani on 5/12/23.
 //
-
+///Create a Task model to represent a task with a title, description, and completion status.
+///
 import Foundation
+struct Task: Identifiable, Equatable {
+    let id = UUID()
+    var title: String
+    var description: String
+    var isCompleted: Bool
+}
--- TaskManager/TaskViewModel.swift
+++ TaskManager/TaskViewModel.swift
@@ -4,5 +4,30 @@
 //
 //  Created by e.shirashiyani on 5/12/23.
 //
+///Create a view model to manage tasks and handle adding, updating, and filtering tasks.
 
-import Foundation
+import SwiftUI
+import Combine
+
+class TaskViewModel: ObservableObject {
+    @Published var tasks: [Task] = []
+    
+    func addTask(title: String, description: String) {
+        let task = Task(title: title, description: description, isCompleted: false)
+        tasks.append(task)
+    }
+    
+    func updateTask(task: Task) {
+        if let index = tasks.firstIndex(of: task) {
+            tasks[index].isCompleted.toggle()
+        }
+    }
+    
+    func filterTasks(isCompleted: Bool) -> [Task] {
+        return tasks.filter { $0.isCompleted == isCompleted }
+    }
+    
+    func moveTask(fromOffsets: IndexSet, toOffset: Int) {
+        tasks.move(fromOffsets: fromOffsets, toOffset: toOffset)
+    }
+}
